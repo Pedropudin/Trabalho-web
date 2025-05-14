cf87c3048e335c7f1351bbf396ca0acf8862ea6d
Pedropudin <pedrofapen@gmail.com>
2025-05-14 20:38:10 -0300

    Add footer component

--- eletrocurte-se/src/App.js
+++ eletrocurte-se/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Eletrocurte-se</p>
+        <Footer />
       </header>
     </div>
   );
