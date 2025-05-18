243f713a89305c947c6c12a2b08780a7aeb628f8
Pedropudin <pedrofapen@gmail.com>
2025-05-18 11:55:05 -0300

    Remove footer from app
    The footer element was used in app for development, now that it works it
    will be used in the apropriate pages

--- eletrocurte-se/src/App.js
+++ eletrocurte-se/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Eletrocurte-se</p>
-        <Footer />
       </header>
     </div>
   );
