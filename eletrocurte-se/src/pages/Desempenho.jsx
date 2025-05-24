6304a7daebe720659da419463e0d426136229a18
Pedropudin <pedrofapen@gmail.com>
2025-05-24 11:16:50 -0300

    Update "Desempenho" page
    Add reputation card and local fetch data

--- eletrocurte-se/src/pages/Desempenho.jsx
+++ eletrocurte-se/src/pages/Desempenho.jsx
@@ -1,17 +1,32 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { data, useNavigate } from "react-router-dom";
 import ROUTES from '../routes';
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Reputation from "../components/Dashboard/Reputation";
+import "../styles/Dashboard.css"
 
 const Desempenho = () => {
     const navigate = useNavigate();
+    const [data, setData] = useState(null);
     const categories = ["Desempenho", "Produtos", "Pendências"];
+
+    /* Provisory local data */
+    useEffect(() => {
+        fetch('/data/dashboard.json')
+        .then((resp) => {
+            return resp.json();
+        })
+        .then(dash_data => {
+            setData(dash_data);
+        })
+    }, []);
 
     
     return(
         <div>
             <Header categories={categories} useElementsMenu={[true, false, true]}/>
+            {data && <Reputation data={data}/>}
             <Footer />
         </div>
     );
