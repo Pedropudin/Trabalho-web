927efc1545ac225e91760452ca8edf86944eead9
Pedropudin <pedrofapen@gmail.com>
2025-05-26 20:20:29 -0300

    Add question page on "Pendencias" page

--- eletrocurte-se/src/components/Pendencias/Question.jsx
+++ eletrocurte-se/src/components/Pendencias/Question.jsx
@@ -1,25 +1,26 @@
 import React from "react";
+import Button from "../Button";
 import "../../styles/Question.css";
 
 const Question = ({ data }) => {
 
     return(
         <div className="question-container">
-            <img src={data.product_photo} width='120px' />
+            <img src={data.product_photo} style={{width: '10%'}} />
             <div className="question-product-info">
                 <span className="product-name">{data.product_name}</span>
-                <div>
-                    <button>A</button>
-                    <button>B</button>
+                <div className="question-buttons-container">
+                    <Button type={1} text="Responder" />
+                    <Button type={1} text="Ver produto" />
                 </div>
                 <span className="question-date">{data.date}</span>
             </div>
             <div className="question-text">
                 {data.question}
             </div>
-            <div className="question-person-info"> 
+            <div className="question-person-info">
                 <span className="question-person-name">{data.person_name}</span>
-                <img src={data.person_photo} width='90px' />
+                <img src={data.person_photo} style={{height: '80%'}} />
             </div>
         </div>
     );
--- eletrocurte-se/src/pages/Pendencias.jsx
+++ eletrocurte-se/src/pages/Pendencias.jsx
@@ -3,13 +3,34 @@ import { useNavigate } from "react-router-dom";
 import ROUTES from '../routes';
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Card from "../components/Card";
+import Question from "../components/Pendencias/Question";
 
 const Pendencias = () => {
+    const [questionsData, setQuestionsData] = useState(null);
     const categories = ["Desempenho", "Produtos", "Pendências"];
-    
+
+    useEffect(() => {
+        fetch("/data/questions.json")
+        .then(resp => {
+            return resp.json();
+        })
+        .then(respData => {
+            setQuestionsData(respData);
+        })
+    }, []);
+
     return(
         <div>
-            <Header categories={categories} useElementsMenu={[true, false, true]}/>
+            <Header categories={categories} useElementsMenu={[true, false, true]} selectedCategoryIndex={2}/>
+            <div className="content">
+                <Card title={"Perguntas"} type={"card-vertical"}>
+                    {questionsData && questionsData.questions.map((q) => {
+                        return <Question data={q} />
+                        //Unique key warning
+                    })}
+                </Card>
+            </div>
             <Footer />
         </div>
     );
