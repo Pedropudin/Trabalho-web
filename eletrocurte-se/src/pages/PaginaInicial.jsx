4cc4c0784d1c09f3b358a8e2a328460de743c0bb
Davi Gabriel <domingues.gabriel.davi@gmail.com>
2025-05-28 10:54:27 -0300

    Consertando imports

--- eletrocurte-se/src/pages/PaginaInicial.jsx
+++ eletrocurte-se/src/pages/PaginaInicial.jsx
@@ -9,9 +9,6 @@ import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import '../styles/PaginaInicial.css';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import ScrollToTop from '../components/ScrollToTop';
 import ROUTES from '../routes';
 
 const categorias = ['Hardware', 'Periféricos', 'Computadores', 'Celulares'];
