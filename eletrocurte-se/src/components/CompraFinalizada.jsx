00199ba9dcb4aac186fbd4924c46c399e3d4427d
Giovanna Noventa <164910701+ginoventa@users.noreply.github.com>
2025-05-30 01:11:52 -0300

    Roteamento correto para página inicial

--- eletrocurte-se/src/components/CompraFinalizada.jsx
+++ eletrocurte-se/src/components/CompraFinalizada.jsx
@@ -8,10 +8,10 @@ export default function CompraFinalizada(){
     return(
         <>
             <div className="conteudo-principal">
-                <strong><h1 className="textos-confirmacao">Pedido feito com sucesso!</h1></strong>
+                <strong><h1 className="textos-confirmacao-cima">Pedido feito com sucesso!</h1></strong>
                 <img className="foto-confirmacao" src="/compra-finalizada.png"></img>
-                <strong><h1 className="textos-confirmacao">Os status do seu pedido são atualizados em tempo real.<br /> Cheque o email ou o a aba “perfil” para ler o andamento de seu pedido. </h1></strong>
-                <button className="botao-inicio" onClick={() => navigate(ROUTES.PAG_)}>Voltar ao início</button>
+                <strong><h1 className="textos-confirmacao-baixo">Os status do seu pedido são atualizados em tempo real.<br /> Cheque o email ou o a aba “perfil” para ler o andamento de seu pedido. </h1></strong>
+                <button className="botao-inicio" onClick={() => navigate(ROUTES.PAG_INICIAL)}>Voltar ao início</button>
             </div>
         </>
     )
