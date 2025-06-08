a8e6afb24f77d67405d58fe763de3ad95e663f2e
Davi Gabriel <domingues.gabriel.davi@gmail.com>
2025-06-08 15:10:02 -0300

    Adição de schema para o admin

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,24 +1,30 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema({
+const clientSchema = new mongoose.Schema({
   nome: String,
   email: { type: String, unique: true },
   senha: String,
-  tipo: { type: String, enum: ['cliente', 'admin'], default: 'cliente' },
-  // outros campos conforme necessário
+  CPF: { type: String, unique: true },
+});
+
+const adminSchema = new mongoose.Schema({
+  nome: String,
+  email: { type: String, unique: true },
+  senha: String,
+  token: { type: Number, unique: true },
 });
 
 // Hash da senha antes de salvar
-userSchema.pre('save', async function (next) {
+clientSchema.pre('save', async function (next) {
   if (!this.isModified('senha')) return next();
   this.senha = await bcrypt.hash(this.senha, 10);
   next();
 });
 
 // Método para comparar senha
-userSchema.methods.compararSenha = function (senha) {
+clientSchema.methods.compararSenha = function (senha) {
   return bcrypt.compare(senha, this.senha);
 };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Client', clientSchema);
