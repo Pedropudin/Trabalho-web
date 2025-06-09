c77910d1448a06b4bd6e4ba55d0d064328ea2ccd
Davi Gabriel <domingues.gabriel.davi@gmail.com>
2025-06-09 00:02:27 -0300

    Planejamento inicial de schema NoSQL para produtos

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({ // Schema a ser definido, conforme JSON final dos produtos
   nome: String,
   marca: String,
   preco: Number,
   descricao: String,
   img: String,
-  inStock: Number,
+  estoque: Number,
+  id: Number,
+  setorGeral: String,
+  setorEspecifico: String,
+  thumbs: [String],
+  descricao: String,
+  descricaoCompleta: String,
+  specifications: [String],
+  avaliacao: String,
+  cor: String,
+  garantia: String,
   // outros campos conforme necessário
 });
 
